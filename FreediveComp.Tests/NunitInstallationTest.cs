a6463b2a5b6f37e22ed1252b5acfa88ab41c5f30
Milan Wilczak <milan.wilczak@gmail.com>
2018-04-12 00:20:12 +0200

    Added project for unit tests

---
+++ FreediveComp.Tests/NunitInstallationTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilanWilczak.FreediveComp.Tests
+{
+    [TestFixture]
+    public class NunitInstallationTest
+    {
+        [Test]
+        public void TestFail()
+        {
+            Assert.That(5, Is.EqualTo(3));
+        }
+    }
+}
---
+++ FreediveComp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FreediveComp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FreediveComp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c6fd643-4372-4c5d-89d1-a230db885637")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
